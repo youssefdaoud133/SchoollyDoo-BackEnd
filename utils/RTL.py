f3495c05db7eff0f6c523a76613aa339acad9b80
YoussefDaoud <youssefdaoud0122@gmail.com>
2023-09-05 02:17:46 +0300

    test

--- utils/RTL.py
+++ utils/RTL.py
@@ -1,7 +1,6 @@
 import sys
 
-# Add the directory containing Pillow to the Python path
-sys.path.append('../../../../../opt/render/.local/lib/python3.7/site-packages')
+
 
 # Now you can import Pillow
 from PIL import Image, ImageDraw, ImageFont
